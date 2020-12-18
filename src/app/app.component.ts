53e1b31043badf8c79a2c3a3b5dc47cd61928d16
Rohit S. Patil <Patil_Rh@India.XoriantCorp.com>
2020-12-18 15:59:34 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SimpleGreeting } from '../app/custom-components/filter-array';
+
+// VVIMP line to render lit element
+console.assert(SimpleGreeting !== undefined);
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lit-element-with-angular';
+  title = 'Welcome to Lit Element';
+  rankingsInSSCExam = ["Rohit", 80, "Abhi", 90];
 }
---
+++ src/app/custom-components/filter-array.ts
@@ -0,0 +1,29 @@
+import { LitElement, html, property, customElement } from 'lit-element';
+
+@customElement('simple-greeting')
+export class SimpleGreeting extends LitElement {
+
+  @property({type: Array}) rankings: any[] = [];
+  @property() title: string;
+  numbers: any[] = [];
+  strings: any[] = [];
+
+  constructor(){
+      super();
+  }
+
+   arrayManipulation(){
+    this.rankings.forEach((value) => {
+        value == Number(value) ? this.numbers.push(value)  : this.strings.push(value);
+    });
+  }
+  
+  render() {
+    this.arrayManipulation();
+    return html`
+        <h1>${this.title}</h1>
+        <p>Ranking Array: ${this.rankings}</p>
+        <p>Numbers Array ${this.numbers.map(item => html`<li> ${item}</li>`)}</p>
+        <p>Strings Array ${this.strings.map(item => html`<li> ${item}</li>`)}</p>`;
+  }
+}
